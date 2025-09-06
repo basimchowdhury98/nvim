3c1252669ee3f64ee8ca705c9a150473ea5fdb8f
basimchowdhury <basimchowdhury@gmail.com>
2025-09-06 15:00:49 -0400

    Harpoon shortcuts

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -4,12 +4,16 @@ local map = vim.keymap.set
 
 map("n", "<leader>e", vim.cmd.Ex)
 
-local mark = require("harpoon.mark")
-local ui = require("harpoon.ui")
-map("n", "<leader>a", mark.add_file);
-map("n", "<C-h>", ui.toggle_quick_menu);
-
 local builtin = require('telescope.builtin')
 map('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
 map('n', '<leader>fg', builtin.git_files, {})
 map('n', '<leader>fs', builtin.live_grep, {})
+
+local mark = require("harpoon.mark")
+local ui = require("harpoon.ui")
+map("n", "<leader>a", mark.add_file)
+map("n", "<leader>h", ui.toggle_quick_menu)
+map("n", "<C-j>", function() ui.nav_file(1) end)
+map("n", "<C-k>", function() ui.nav_file(2) end)
+map("n", "<C-l>", function() ui.nav_file(3) end)
+
