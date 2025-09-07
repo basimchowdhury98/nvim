7fd9d051751bd0aeea626d54d06c3e6b23c45269
basimchowdhury <basimchowdhury@gmail.com>
2025-09-06 21:23:00 -0400

    remaps + sets

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -3,6 +3,14 @@ vim.g.maplocalleader = "\\"
 local map = vim.keymap.set
 
 map("n", "<leader>e", vim.cmd.Ex)
+map("v", "J", ":m '>+1<CR>gv=gv")
+map("v", "K", ":m '<-2<CR>gv=gv")
+map("n", "<C-d>", "<C-d>zz")
+map("n", "<C-u>", "<C-u>zz")
+map("n", "n", "nzzzv")
+map("n", "N", "Nzzzv")
+map("n", "<leader>y", "\"+y")
+map("v", "<leadery", "\"+y")
 
 local builtin = require('telescope.builtin')
 map('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
