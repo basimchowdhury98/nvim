25a4c5803d86e7b1d7f659bc6fc3cd071ddfa1fc
basimchowdhury <basimchowdhury@gmail.com>
2025-09-08 00:18:48 -0400

    autocomplete init

--- lua/config/plugins/lsp.lua
+++ lua/config/plugins/lsp.lua
@@ -1,27 +1,32 @@
 return {
     {
-       "mason-org/mason.nvim",
+        "mason-org/mason.nvim",
         config = function()
-	        require("mason").setup()
+            require("mason").setup()
         end
     },
     {
-	"mason-org/mason-lspconfig.nvim",
-	config = function()
-	    require("mason-lspconfig").setup({
-            ensure_installed = {
-                "lua_ls",
-                "ts_ls"
-            }
-	    })
-	end
+        "mason-org/mason-lspconfig.nvim",
+        config = function()
+            require("mason-lspconfig").setup({
+                ensure_installed = {
+                    "lua_ls",
+                    "ts_ls"
+                }
+            })
+        end
     },
     {
-	"neovim/nvim-lspconfig",
-	config = function()
-	    local lspconfig = require("lspconfig")
-	    lspconfig.lua_ls.setup({})
-        lspconfig.ts_ls.setup({})
-	end
+        "neovim/nvim-lspconfig",
+        config = function()
+            local capabilities = require("cmp_nvim_lsp").default_capabilities()
+            local lspconfig = require("lspconfig")
+            lspconfig.lua_ls.setup({
+                capabilities = capabilities
+            })
+            lspconfig.ts_ls.setup({
+                capabilities = capabilities
+            })
+        end
     }
 }
