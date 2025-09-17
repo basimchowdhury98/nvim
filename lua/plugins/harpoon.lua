65c8d7a59ca1650d61d2491c0ff5142b8ae4fdc7
Basim Chowdhury <bchowdhury@softheon.com>
2025-09-17 10:26:20 -0400

    Wrapped harpoon add file to print when file added

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -1,3 +1,13 @@
 return {
     "ThePrimeagen/harpoon",
+    config = function()
+        -- Monkey patching/wrapping original add file method to print the file thats being added
+        local mark = require('harpoon.mark')
+        local original_add_file = mark.add_file
+        mark.add_file = function(...)
+            local ret = original_add_file(...)
+            print("Harpooned: " .. vim.fn.expand("%"))
+            return ret
+        end
+    end
 }
