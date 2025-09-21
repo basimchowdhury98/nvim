50610abf7a32d44b653f08728c1f58bd703df7f2
basimchowdhury <basimchowdhury@gmail.com>
2025-09-21 09:06:32 -0400

    Gruvbox mat colors

--- lua/plugins/colors.lua
+++ lua/plugins/colors.lua
@@ -1,22 +1,30 @@
 return {
-  {
-    "folke/tokyonight.nvim",
-    lazy = false,
-    priority = 1000,
-    opts = {
-        transparent = true
+    {
+        "folke/tokyonight.nvim",
+        lazy = false,
+        priority = 1000,
+        opts = {
+            transparent = true
+        },
     },
-  },
-  { 'nvim-mini/mini.icons', version = false },
-  {
-      "prichrd/netrw.nvim",
-      opts = {},
-      config = function()
-          require("mini.icons").setup()
-          require("netrw").setup({
-              use_devicons = true,
-              -- other options
-          })
-      end
-  }
+    {
+        'sainnhe/gruvbox-material',
+        lazy = false,
+        priority = 1000,
+        config = function()
+            vim.g.gruvbox_material_enable_italic = true
+        end
+    },
+    { 'nvim-mini/mini.icons', version = false },
+    {
+        "prichrd/netrw.nvim",
+        opts = {},
+        config = function()
+            require("mini.icons").setup()
+            require("netrw").setup({
+                use_devicons = true,
+                -- other options
+            })
+        end
+    }
 }
--- lua/set.lua
+++ lua/set.lua
@@ -1,4 +1,4 @@
-vim.cmd.colorscheme("tokyonight")
+vim.cmd.colorscheme("gruvbox-material")
 
 vim.opt.nu = true
 vim.opt.relativenumber = true
