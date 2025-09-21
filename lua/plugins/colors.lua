c6aeb2dc5f5baf95100b32e0af0b8f869d4fe4f5
Basim Chowdhury <bchowdhury@softheon.com>
2025-09-21 09:47:40 -0400

    gruvbox config

--- lua/plugins/colors.lua
+++ lua/plugins/colors.lua
@@ -13,6 +13,12 @@ return {
         priority = 1000,
         config = function()
             vim.g.gruvbox_material_enable_italic = true
+            vim.g.gruvbox_material_foreground = 'mix'
+            vim.g.gruvbox_material_background = 'hard'
+            vim.g.gruvbox_material_ui_contrast = 'high'
+            vim.g.gruvbox_material_float_style = 'bright'
+            vim.g.gruvbox_material_statusline_style = 'material'
+            vim.g.gruvbox_material_cursor = 'auto'
         end
     },
     { 'nvim-mini/mini.icons', version = false },
