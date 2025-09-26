ec0507dac0916d3e471b62851f0ff53d01608060
Basim <basimchowdhury@gmail.com>
2025-09-25 20:49:10 -0400

    some sets

--- lua/set.lua
+++ lua/set.lua
@@ -1,24 +1,21 @@
 vim.cmd.colorscheme("tokyonight")
-
-vim.opt.nu = true
 vim.opt.relativenumber = true
-
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
-
 vim.opt.wrap = false
-
 vim.opt.hlsearch = false
 vim.opt.incsearch = true
-
 vim.opt.scrolloff = 8
-
 vim.opt.updatetime = 50
-
 vim.opt.colorcolumn = "120"
-
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.nu = true
+vim.opt.updatetime = 250
+vim.opt.timeoutlen = 300
+vim.opt.confirm = true
 vim.diagnostic.config({
     virtual_lines = {
         current_line = true
