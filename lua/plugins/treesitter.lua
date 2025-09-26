8c7bad2a9d4df8a7468b525ca20c3f7dc2b3840c
Basim <basimchowdhury@gmail.com>
2025-09-25 22:58:14 -0400

    treesitter context

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,24 +1,36 @@
 return {
-    "nvim-treesitter/nvim-treesitter",
-    branch = 'master',
-    lazy = false,
-    build = ":TSUpdate",
-    main = 'nvim-treesitter.configs',
-    opts = {
-	  -- A list of parser names, or "all" (the listed parsers MUST always be installed)
-	  ensure_installed = { "c", "lua", "vim", "vimdoc", "markdown", "markdown_inline", "typescript", "javascript", "tsx", "c_sharp"},
+    {
+        "nvim-treesitter/nvim-treesitter",
+        branch = 'master',
+        lazy = false,
+        build = ":TSUpdate",
+        main = 'nvim-treesitter.configs',
+        opts = {
+            ensure_installed = { "c", "lua", "vim", "vimdoc", "markdown", "markdown_inline", "typescript", "javascript", "tsx", "c_sharp" },
 
-	  -- Install parsers synchronously (only applied to `ensure_installed`)
-	  sync_install = false,
+            sync_install = false,
 
-	  -- Automatically install missing parsers when entering buffer
-	  -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-	  auto_install = true,
+            auto_install = true,
 
-	  highlight = {
-	    enable = true,
+            highlight = {
+                enable = true,
 
-	    additional_vim_regex_highlighting = false,
-	  },
-	}
+                additional_vim_regex_highlighting = false,
+            },
+        }
+    },
+    {
+        'nvim-treesitter/nvim-treesitter-context',
+        config = function()
+            require('treesitter-context').setup({
+                enable = true,
+                max_lines = 3, -- How many context lines to show
+                min_window_height = 0,
+                line_numbers = true,
+                multiline_threshold = 2, -- Maximum number of lines to show for a single context
+                trim_scope = 'outer', -- Which context lines to discard if `max_lines` is exceeded
+                mode = 'cursor',     -- Line used to calculate context. Choices: 'cursor', 'topline'
+            })
+        end
+    }
 }
