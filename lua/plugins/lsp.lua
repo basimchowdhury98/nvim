ed41fefe441dcf923e4ecd4fe7d9ba4f8c505a44
Basim Chowdhury <bchowdhury@softheon.com>
2025-09-28 13:46:27 -0400

    Reorganized lsp config + harpoon flash

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,18 +1,42 @@
 return {
     {
-        "mason-org/mason.nvim",
+        "neovim/nvim-lspconfig",
+        event = 'VeryLazy',
+        dependencies = {
+            "mason-org/mason-lspconfig.nvim",
+            "L3MON4D3/LuaSnip",
+            "mason-org/mason.nvim",
+            'folke/lazydev.nvim',
+            "rafamadriz/friendly-snippets", --premade snippets
+            {
+                'saghen/blink.cmp',
+                opts = {
+                    keymap = {
+                        preset = 'default'
+                    },
+                    appearance = {
+                        nerd_font_variant = 'mono'
+                    },
+                    sources = {
+                        default = { 'lsp', 'path', 'snippets', 'lazydev' },
+                        providers = {
+                            lazydev = { module = 'lazydev.integrations.blink', score_offset = 100 }
+                        }
+                    },
+                    snippets = { preset = 'luasnip' },
+                    signature = { enabled = true },
+                    fuzzy = { implementation = 'lua' }
+                }
+            },
+        },
         config = function()
+            require("luasnip.loaders.from_vscode").lazy_load()
             require("mason").setup({
                 registries = {
                     "github:mason-org/mason-registry",
                     "github:Crashdummyy/mason-registry",
                 }
             })
-        end
-    },
-    {
-        "mason-org/mason-lspconfig.nvim",
-        config = function()
             require("mason-lspconfig").setup({
                 ensure_installed = {
                     "lua_ls",
@@ -20,7 +44,7 @@ return {
                 },
                 handlers = {
                     function(server_name)
-                        local capabilities = require("cmp_nvm_lsp").default_capabilities()
+                        local capabilities = require('blink.cmp').get_lsp_capabilities()
                         require("lspconfig")[server_name].setup({
                             capabilities = capabilities
                         })
@@ -30,10 +54,8 @@ return {
         end
     },
     {
-        "neovim/nvim-lspconfig"
-    },
-    {
         "seblyng/roslyn.nvim",
+        ft = { "cs" },
         ---@module 'roslyn.config'
         ---@type RoslynNvimConfig
         opts = {
