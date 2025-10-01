9b5ad7a257d4723f9f7dd87c0c36b5ead89cf087
basimchowdhury <basimchowdhury@gmail.com>
2025-09-30 21:50:25 -0400

    Wezterm os based default_prog

--- wezterm.lua
+++ wezterm.lua
@@ -4,7 +4,12 @@ local act = wezterm.action
 local config = {}
 if wezterm.config_builder then config = wezterm.config_builder() end
 
-config.default_prog = { 'powershell.exe' }
+if wezterm.target_triple == "x86_64-pc-windows-msvc" or wezterm.target_triple == "aarch64-pc-windows-msvc" then
+  config.default_prog = { 'powershell.exe' }
+elseif wezterm.target_triple == "x86_64-apple-darwin" or wezterm.target_triple == "aarch64-apple-darwin" then
+  config.default_prog = { '/bin/zsh' }
+end
+
 config.color_scheme = "Tokyo Night"
 config.window_background_opacity = 0.9
 config.window_decorations = "RESIZE"
