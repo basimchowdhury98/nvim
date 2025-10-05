9a4328d84a1c26e2b00e6bd2cb18bd061ee97dd5
Basim <basimchowdhury@gmail.com>
2025-10-04 22:49:28 -0400

    wezterm multiplexing

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -30,19 +30,104 @@ config.background = {
 }
 
 
-config.leader = { key = "q", mods = "ALT", timeout_millisecionds = 2000 }
+config.leader = { key = "`", mods = "ALT", timeout_millisecionds = 2000 }
 config.keys = {
     {
         mods = "LEADER",
-        key = "c",
+        key = "i",
         action = wezterm.action.SpawnTab "CurrentPaneDomain"
     },
     {
         mods = "LEADER",
-        key = "x",
+        key = ";",
         action = wezterm.action.CloseCurrentPane { confirm = true }
-    }
+    },
+    {
+        mods = "LEADER",
+        key = "p",
+        action = wezterm.action.ActivateTabRelative(-1)
+    },
+    {
+        mods = "LEADER",
+        key = "n",
+        action = wezterm.action.ActivateTabRelative(1)
+    },
+    {
+        mods = "LEADER",
+        key = "\\",
+        action = wezterm.action.SplitHorizontal { domain = "CurrentPaneDomain" }
+    },
+    {
+        mods = "LEADER",
+        key = "-",
+        action = wezterm.action.SplitVertical { domain = "CurrentPaneDomain" }
+    },
+    {
+        mods = "LEADER",
+        key = "h",
+        action = wezterm.action.ActivatePaneDirection "Left"
+    },
+    {
+        mods = "LEADER",
+        key = "j",
+        action = wezterm.action.ActivatePaneDirection "Down"
+    },
+    {
+        mods = "LEADER",
+        key = "k",
+        action = wezterm.action.ActivatePaneDirection "Up"
+    },
+    {
+        mods = "LEADER",
+        key = "l",
+        action = wezterm.action.ActivatePaneDirection "Right"
+    },
+    {
+        mods = "LEADER",
+        key = "LeftArrow",
+        action = wezterm.action.AdjustPaneSize { "Left", 2 }
+    },
+    {
+        mods = "LEADER",
+        key = "RightArrow",
+        action = wezterm.action.AdjustPaneSize { "Right", 2 }
+    },
+    {
+        mods = "LEADER",
+        key = "DownArrow",
+        action = wezterm.action.AdjustPaneSize { "Down", 2 }
+    },
+    {
+        mods = "LEADER",
+        key = "UpArrow",
+        action = wezterm.action.AdjustPaneSize { "Up", 2 }
+    },
+    {
+        mods = "LEADER",
+        key = "f",
+        action = wezterm.action_callback(function (win, pane)
+           local tab = win:active_tab()
+           for _, p in ipairs(tab:panes()) do
+            if p:pane_id() ~= pane:pane_id() then
+                p:activate()
+                win:perform_action(wezterm.action.CloseCurrentPane { confirm = true }, p)
+            end
+           end
+        end)
+    },
 }
+
+for i = 1, 9 do
+    table.insert(config.keys, {
+        key = tostring(i),
+        mods = "LEADER",
+        action = wezterm.action.ActivateTab(i - 1)
+    })
+end
+
+config.tab_bar_at_bottom = true
+config.use_fancy_tab_bar = false
+config.hide_tab_bar_if_only_one_tab = true
 
 wezterm.on("update-right-status", function (window, _)
     local text = ""
