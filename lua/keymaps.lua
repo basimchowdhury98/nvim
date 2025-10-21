f58c40adcf03d2e68132c2f6c85d3db869c63c3d
Basim Chowdhury <bchowdhury@softheon.com>
2025-10-21 19:47:29 -0400

    split i,t map for jke

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -44,7 +44,8 @@ map("i", "<C-l>", "<Esc>la", { desc = "Move one char left in insert mode" })
 map("i", "<S-CR>", "<Esc>$o", { desc = "From anywhere in line enter into a new line in insert mode" })
 
 -- Buffer
-map({ "i", "t" }, "jke", "<C-c>", { desc = "[E]xit from insert/terminal to normal mode", remap = true })
+map({ "i" }, "jke", "<C-c>", { desc = "[E]xit from insert to normal mode", remap = true })
+map({ "t" }, "jke", "<Esc>", { desc = "[E]xit from terminal to normal mode", remap = true })
 map({ "i" }, "jka", "<C-c>A", { desc = "[A]ppend to end of line"})
 map({ "i" }, "jko", "<C-c>O", { desc = "[] New line above ie Shift+o"})
 
