c4b0162f9d004537b7eff075029a20024a32e677
basimchowdhury <basimchowdhury@gmail.com>
2025-11-09 00:47:44 -0500

    + term tests

--- specs/terminal_spec.lua
+++ specs/terminal_spec.lua
@@ -20,6 +20,8 @@ describe("Floating terminal", function()
                 pcall(vim.api.nvim_buf_delete, buf, { force = true })
             end
         end
+
+        vim.fn.getcwd = function() return "/stub/proj" end
     end)
 
     it("opens the terminal when none exists", function()
@@ -42,6 +44,7 @@ describe("Floating terminal", function()
 
     it("processes opens idempotently", function()
         local win_count_before = #vim.api.nvim_list_wins()
+        local buf_count_before = #vim.api.nvim_list_bufs()
 
         local _ = term.open()
         local win_id = term.open()
@@ -51,8 +54,26 @@ describe("Floating terminal", function()
         assert(buf_id ~= nil)
         local win_count_after = #vim.api.nvim_list_wins()
         eq(win_count_after, win_count_before + 1, "One new window should be open after")
+        local buf_count_after = #vim.api.nvim_list_bufs()
+        eq(buf_count_after, buf_count_before +1, "Only one new buffer is open after")
         local term_job_id = vim.b[buf_id].terminal_job_id
         assert(term_job_id ~= nil and term_job_id > 0, "Term job should have started")
+    end)
+
+    it("opens new terminal in new project", function()
+        local win_id_proj1 = term.open()
+        local buf_id_proj1 = vim.api.nvim_win_get_buf(win_id_proj1)
+        term.close()
+        vim.fn.getcwd = function() return "/stub2/newproj" end
+        local win_id_proj2 = term.open()
+        local buf_id_proj2 = vim.api.nvim_win_get_buf(win_id_proj2)
+
+        assert(win_id_proj1 ~= nil)
+        assert(win_id_proj2 ~= nil)
+        assert(buf_id_proj1 ~= nil)
+        assert(buf_id_proj2 ~= nil)
+        assert(win_id_proj1 ~= win_id_proj2, "Different windows since window needs to be closed first")
+        assert(buf_id_proj1 ~= buf_id_proj2, "Different buffer per project")
     end)
 
     it("closes the window when one is open", function ()
@@ -96,7 +117,6 @@ describe("Floating terminal", function()
         assert(term_job_id ~= nil and term_job_id > 0, "Term job should have started")
     end)
 
-
     it("opens 2 new terminals when_open_new terminal called twice", function ()
         local win_count_before = #vim.api.nvim_list_wins()
         local buf_count_before = #vim.api.nvim_list_bufs()
@@ -121,5 +141,100 @@ describe("Floating terminal", function()
         local term_job_id_2 = vim.b[buf_id_2].terminal_job_id
         assert(term_job_id_2 ~= nil and term_job_id_2 > 0, "Term job should have started")
         assert(term_job_id ~= term_job_id_2)
+    end)
+
+    it("can rotate to the prev terminal", function ()
+        local win_id = term.open_new_terminal()
+        local first_buf_id = vim.api.nvim_win_get_buf(win_id)
+        term.open_new_terminal()
+        local second_buf_id = vim.api.nvim_win_get_buf(win_id)
+
+        term.prev()
+
+        assert(first_buf_id ~= second_buf_id)
+        local curr_buf_id = vim.api.nvim_win_get_buf(win_id)
+        eq(curr_buf_id, first_buf_id, "After prev win should go back to first buff")
+    end)
+
+    it("can rotate to the last terminal if preving from first terminal", function ()
+        local win_id = term.open_new_terminal()
+        local first_buf_id = vim.api.nvim_win_get_buf(win_id)
+        term.open_new_terminal()
+        local second_buf_id = vim.api.nvim_win_get_buf(win_id)
+
+        term.prev() --puts win on first term
+        term.prev()
+
+        assert(first_buf_id ~= second_buf_id)
+        local curr_buf_id = vim.api.nvim_win_get_buf(win_id)
+        eq(curr_buf_id, second_buf_id, "After prev when at the beginning win should go wrap back to last buff")
+    end)
+
+    it("can rotate to the next terminal", function ()
+        local win_id = term.open_new_terminal()
+        local first_buf_id = vim.api.nvim_win_get_buf(win_id)
+        term.open_new_terminal()
+        local second_buf_id = vim.api.nvim_win_get_buf(win_id)
+
+        term.prev() -- to set the term back to first
+        term.next()
+
+        assert(first_buf_id ~= second_buf_id)
+        local curr_buf_id = vim.api.nvim_win_get_buf(win_id)
+        eq(curr_buf_id, second_buf_id, "After next win should go forward to second buff")
+    end)
+
+    it("can rotate to the first terminal when next at the last terminal", function ()
+        local win_id = term.open_new_terminal()
+        local first_buf_id = vim.api.nvim_win_get_buf(win_id)
+        term.open_new_terminal()
+        local second_buf_id = vim.api.nvim_win_get_buf(win_id)
+
+        term.next()
+
+        assert(first_buf_id ~= second_buf_id)
+        local curr_buf_id = vim.api.nvim_win_get_buf(win_id)
+        eq(curr_buf_id, first_buf_id, "After next win at the last terminal it wraps back around to first")
+    end)
+
+    it("only rotates between project terminals", function ()
+        vim.fn.getcwd = function() return "/stub/diffproj" end
+        local diff_proj_win = term.open()
+        local diff_proj_buf_id = vim.api.nvim_win_get_buf(diff_proj_win)
+        term.close()
+        vim.fn.getcwd = function() return "stub/thisproj" end
+        local win_id = term.open_new_terminal()
+        term.open_new_terminal()
+
+        local curr_buf_id = vim.api.nvim_win_get_buf(win_id)
+        assert(curr_buf_id ~= diff_proj_buf_id)
+        term.next()
+        curr_buf_id = vim.api.nvim_win_get_buf(win_id)
+        assert(curr_buf_id ~= diff_proj_buf_id)
+        term.next()
+        curr_buf_id = vim.api.nvim_win_get_buf(win_id)
+        assert(curr_buf_id ~= diff_proj_buf_id)
+        term.prev()
+        curr_buf_id = vim.api.nvim_win_get_buf(win_id)
+        assert(curr_buf_id ~= diff_proj_buf_id)
+        term.prev()
+        curr_buf_id = vim.api.nvim_win_get_buf(win_id)
+        assert(curr_buf_id ~= diff_proj_buf_id)
+    end)
+
+    it("kills terminals", function ()
+        local initial_bufs = #vim.api.nvim_list_bufs()
+        term.open_new_terminal()
+        term.open_new_terminal()
+        vim.fn.getcwd = function() return "/stub/diffproj" end
+        term.open_new_terminal()
+        term.open_new_terminal()
+        local bufs_after_opens = #vim.api.nvim_list_bufs()
+
+        term.kill()
+
+        local bufs_after_kill = #vim.api.nvim_list_bufs()
+        eq(bufs_after_opens, initial_bufs + 4, "Opened 4 term bufs")
+        eq(bufs_after_kill, initial_bufs, "Buf count should go back to normal after kill")
     end)
 end)
