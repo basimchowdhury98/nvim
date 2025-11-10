bd179a4b7b8a1356da59a9e65633bf90d0c4a931
Basim Chowdhury <bchowdhury@softheon.com>
2025-11-10 09:57:15 -0500

    Added aliases

--- windows_pwsh/profile.ps1
+++ windows_pwsh/profile.ps1
@@ -1,1 +1,28 @@
-function gs { git status }
+Set-Alias d docker
+
+function di {
+    docker image @args
+}
+
+function dc {
+    docker container @args
+}
+
+Set-Alias g git
+
+function gs {
+    git status
+}
+
+function gap {
+    git add -p
+}
+
+function gc {
+    param([string]$message)
+    git commit -m $message
+}
+
+Remove-PSReadLineKeyHandler -Chord 'Ctrl+v'
+
+oh-my-posh init pwsh --config 'gruvbox' | Invoke-Expression
--- zsh/.zshrc
+++ zsh/.zshrc
@@ -1,1 +1,28 @@
-alias gs="git status"
+# Aliases
+alias d='docker'
+alias g='git'
+
+# Docker functions
+di() {
+    docker image "$@"
+}
+
+dc() {
+    docker container "$@"
+}
+
+# Git functions
+gs() {
+    git status
+}
+
+gap() {
+    git add -p
+}
+
+gc() {
+    git commit -m "$1"
+}
+
+# Oh My Posh (if installed on macOS)
+eval "$(oh-my-posh init zsh --config 'gruvbox')"
