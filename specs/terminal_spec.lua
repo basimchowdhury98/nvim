392dedd80d3f5080d79f3ba505e02887ef719c05
Basim <basimchowdhury@gmail.com>
2025-11-23 16:03:29 -0500

    refactored preload testing

--- specs/terminal_spec.lua
+++ specs/terminal_spec.lua
@@ -1,8 +1,8 @@
 local eq = assert.are.same
-local function findPreloadedTerm(ignoreBuf)
+local function find_term_buffer(term_buf_to_ignore)
     local bufs = vim.api.nvim_list_bufs()
     for _, buf in ipairs(bufs) do
-        if ignoreBuf ~= nil and buf == ignoreBuf then
+        if term_buf_to_ignore ~= nil and buf == term_buf_to_ignore then
             goto continue
         end
         if vim.api.nvim_buf_is_valid(buf) then
@@ -19,13 +19,13 @@ local function findPreloadedTerm(ignoreBuf)
     return nil
 end
 
-local function setupPath(path)
+local function setup_path(path)
     if vim.fn.isdirectory(path) == 0 then
         vim.fn.mkdir(path, 'p')
     end
 end
 
-local function assertTerminalOpened(win_id)
+local function assert_terminal_opened(win_id)
     assert(win_id ~= nil)
     local buf_id = vim.api.nvim_win_get_buf(win_id)
     assert(buf_id ~= nil)
@@ -41,89 +41,90 @@ local function assertTerminalOpened(win_id)
     return buf_id
 end
 
-local function assertAnotherTermPreloaded(buf_to_skip)
-    local preloaded_next_term = findPreloadedTerm(buf_to_skip)
-    assert(preloaded_next_term ~= nil and preloaded_next_term ~= buf_to_skip,
+local function assert_another_preloaded_ignoring(term_buf_to_skip)
+    local preloaded_next_term = find_term_buffer(term_buf_to_skip)
+    assert(preloaded_next_term ~= nil and preloaded_next_term ~= term_buf_to_skip,
         "Didnt preload another term after done")
 end
 
-local termOptSpy
+local term_opt_spy = {}
 local function spyOnTermOpen()
     local orig = vim.fn.termopen
     vim.fn.termopen = function(_, opt)
+        local curr_buf = vim.api.nvim_get_current_buf()
         if opt == nil then
             orig(_)
+            term_opt_spy[curr_buf] = "proj_path"
         else
             orig(_, opt)
+            term_opt_spy[curr_buf] = opt.cwd
         end
-        termOptSpy = opt
     end
 end
 
 local notifySpy
 local function spyOnNotify()
-    vim.notify = function (msg, _)
+    vim.notify = function(msg, _)
         notifySpy = msg
     end
 end
 
-local test_proj = vim.fn.stdpath("cache") .. "\\terminal-test"
-local test_proj_2 = vim.fn.stdpath("cache") .. "\\terminal-test-2"
-setupPath(test_proj)
-setupPath(test_proj_2)
-spyOnTermOpen()
-spyOnNotify()
-
 describe("Floating terminal", function()
     local term = require("utils.terminal")
+    local test_proj = vim.fn.stdpath("cache") .. "\\terminal-test"
+    local test_proj_2 = vim.fn.stdpath("cache") .. "\\terminal-test-2"
+    setup_path(test_proj)
+    setup_path(test_proj_2)
+    spyOnTermOpen()
+    spyOnNotify()
 
     before_each(function()
         term.kill()
         vim.fn.chdir(test_proj)
+        term_opt_spy = {}
+        notifySpy = nil
     end)
 
     it("opens the terminal without setup", function()
         local win_id = term.open()
 
-        local buf_id = assertTerminalOpened(win_id)
-        assert(termOptSpy == nil, "A specific path was passed when it should be pwd/proj path")
-        assertAnotherTermPreloaded(buf_id)
+        local buf_id = assert_terminal_opened(win_id)
+        assert(term_opt_spy[buf_id] ~= nil and term_opt_spy[buf_id] == "proj_path",
+            "A specific path was passed when it should be pwd/proj path")
+        assert_another_preloaded_ignoring(buf_id)
     end)
 
     it("opens window with setup uses a preloaded term", function()
         term.setupForProject()
-        local preloaded_term_buf = findPreloadedTerm()
+        local preloaded_term_buf = find_term_buffer()
 
         local win_id = term.open()
 
-        local buf_id = assertTerminalOpened(win_id)
+        local buf_id = assert_terminal_opened(win_id)
         assert(preloaded_term_buf ~= nil)
         eq(buf_id, preloaded_term_buf, "Didnt use the preloaded term")
-        assertAnotherTermPreloaded(buf_id)
+        assert_another_preloaded_ignoring(buf_id)
     end)
 
     it("processes opens idempotently", function()
         local win_count_before = #vim.api.nvim_list_wins()
-        local buf_count_before = #vim.api.nvim_list_bufs()
 
         local _ = term.open()
         local win_id = term.open()
 
-        assertTerminalOpened(win_id)
+        local buf_id = assert_terminal_opened(win_id)
         local win_count_after = #vim.api.nvim_list_wins()
         eq(win_count_after, win_count_before + 1, "Only one new window should have opened")
-        local buf_count_after = #vim.api.nvim_list_bufs()
-        eq(buf_count_after, buf_count_before + 2,
-            "Exactly 2 bufs should be created, active and preload")
+        assert_another_preloaded_ignoring(buf_id)
     end)
 
     it("opens new terminal in new project", function()
         local win_id_proj1 = term.open()
-        local buf_id_proj1 = assertTerminalOpened(win_id_proj1)
+        local buf_id_proj1 = assert_terminal_opened(win_id_proj1)
         term.close()
         vim.fn.chdir(test_proj_2)
         local win_id_proj2 = term.open()
-        local buf_id_proj2 = assertTerminalOpened(win_id_proj2)
+        local buf_id_proj2 = assert_terminal_opened(win_id_proj2)
 
         assert(win_id_proj1 ~= win_id_proj2, "Same window shouldnt be used for both projects")
         assert(buf_id_proj1 ~= buf_id_proj2, "Same buffer shouldnt be used for both projects")
@@ -155,18 +156,31 @@ describe("Floating terminal", function()
     it("opens a new terminal", function()
         local win_id = term.open_new_terminal()
 
-        assertTerminalOpened(win_id)
+        local buf_id = assert_terminal_opened(win_id)
+        assert_another_preloaded_ignoring(buf_id)
+    end)
+
+    it("opens new terminal with setup uses a preloaded term", function()
+        term.setupForProject()
+        local preloaded_term_buf = find_term_buffer()
+
+        local win_id = term.open_new_terminal()
+
+        local buf_id = assert_terminal_opened(win_id)
+        assert(preloaded_term_buf ~= nil, "A term buffer wasnt preloaded")
+        eq(buf_id, preloaded_term_buf, "Didnt use the preloaded term")
+        assert_another_preloaded_ignoring(buf_id)
     end)
 
     it("opens 2 new terminals when_open_new terminal called twice", function()
         local win_count_before = #vim.api.nvim_list_wins()
 
         local win_id = term.open_new_terminal()
-        local buf_id = assertTerminalOpened(win_id)
+        local buf_id = assert_terminal_opened(win_id)
         local win_id_2 = term.open_new_terminal()
 
         assert(win_id == win_id_2, "Different windows were used when it should be just one")
-        local buf_id_2 = assertTerminalOpened(win_id_2)
+        local buf_id_2 = assert_terminal_opened(win_id_2)
         assert(buf_id ~= buf_id_2, "Same buffer used each time but should be different")
         local win_count_after = #vim.api.nvim_list_wins()
         eq(win_count_after, win_count_before + 1, "More than 1 new window opened")
@@ -267,15 +281,13 @@ describe("Floating terminal", function()
     end)
 
     it("opens terminal at specified path", function()
-        local specificPath = test_proj .. "\\specific"
-        setupPath(specificPath)
+        local specific_path = test_proj .. "\\specific"
+        setup_path(specific_path)
 
-        local win_id = term.open_at_path(specificPath)
+        local win_id = term.open_at_path(specific_path)
 
-        local buf_id = assertTerminalOpened(win_id)
-        assert(buf_id ~= nil)
-        assert(termOptSpy ~= nil and termOptSpy.cwd == specificPath, "Specific path wasnt opened")
-        termOptSpy = nil
+        local buf_id = assert_terminal_opened(win_id)
+        assert(term_opt_spy[buf_id] ~= nil and term_opt_spy[buf_id] == specific_path, "Specific path wasnt opened")
     end)
 
     it("vim notifies when attempt to open specific path outside of project", function()
