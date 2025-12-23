2c6eef3c93627001f3743a031546cef42ef107e0
Basim Chowdhury <bchowdhury@gmail.com>
2025-12-23 16:49:01 -0500

    removed FixCursorHold bc it was needed as a workaround but now its not

--- lua/plugins/test.lua
+++ lua/plugins/test.lua
@@ -4,7 +4,6 @@ return {
         dependencies = {
             "nvim-neotest/nvim-nio",
             "nvim-lua/plenary.nvim",
-            "antoinemadec/FixCursorHold.nvim",
             "Issafalcon/neotest-dotnet",
         },
         config = function()
