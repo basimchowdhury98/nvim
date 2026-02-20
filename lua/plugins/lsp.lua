db41ad61ebb968bdee2881fcb7f500a122fcbd10
User <user@example.com>
2026-02-19 22:00:37 -0500

    updated lsp for new version + cland

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -39,7 +39,7 @@ return {
                             max_height = 10
                         }
                     },
-                    fuzzy = { implementation = 'lua' }
+                    fuzzy = { implementation = 'lua' },
                 }
             },
         },
@@ -71,6 +71,10 @@ return {
             require("luasnip.loaders.from_vscode").lazy_load()
             require("luasnip.loaders.from_lua").lazy_load({ paths = { "./lua/snippets" } })
 
+            vim.lsp.config('*', {
+                capabilities = require('blink.cmp').get_lsp_capabilities()
+            })
+
             require("mason").setup({
                 registries = {
                     "github:mason-org/mason-registry",
@@ -82,15 +86,10 @@ return {
                     "lua_ls",
                     "ts_ls",
                 },
-                handlers = {
-                    function(server_name)
-                        local capabilities = require('blink.cmp').get_lsp_capabilities()
-                        require("lspconfig")[server_name].setup({
-                            capabilities = capabilities
-                        })
-                    end
-                }
             })
+
+            -- Enable servers not managed by mason
+            vim.lsp.enable('clangd')
 
             local telescope = require("telescope.builtin")
             vim.api.nvim_create_autocmd("LspAttach", {
@@ -115,21 +114,13 @@ return {
             vim.api.nvim_create_autocmd("LspAttach", {
                 group = vim.api.nvim_create_augroup("lsp-presentation", { clear = true }),
                 callback = function(event)
-                    local function client_supports_method(client, method, bufnr)
-                        if vim.fn.has 'nvim-0.11' == 1 then
-                            return client:supports_method(method, bufnr)
-                        else
-                            return client.supports_method(method, { bufnr = bufnr })
-                        end
-                    end
-
                     -- Starting semantics token so treesitter can color better
                     local client = vim.lsp.get_client_by_id(event.data.client_id)
                     if client and client.server_capabilities.semanticTokensProvider then
                         vim.lsp.semantic_tokens.start(event.buf, client.id)
                     end
 
-                    if client and client_supports_method(client, vim.lsp.protocol.Methods.textDocument_documentHighlight, event.buf) then
+                    if client and client:supports_method(vim.lsp.protocol.Methods.textDocument_documentHighlight, event.buf) then
                         local highlight_augroup = vim.api.nvim_create_augroup('lsp-highlight', { clear = false })
 
                         -- When cursor stops moving: Highlights all instances of the symbol under the cursor
