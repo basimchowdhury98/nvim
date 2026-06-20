2846745036c877d96e7366bf99adb786770e141c
Basim Chowdhury <bchowdhury@softheon.com>
2026-06-20 14:00:58 -0400

    local config hook

--- init.lua
+++ init.lua
@@ -7,3 +7,9 @@ require("set")
 require("commands")
 require("autocommands")
 require("utils.terminal")
+
+-- Load machine-local config if it exists
+local local_config = vim.fn.stdpath("config"):gsub("nvim$", "nvim-local") .. "/init.lua"
+if vim.uv.fs_stat(local_config) then
+  dofile(local_config)
+end
