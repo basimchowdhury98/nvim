d3fd0d97c0c68d0825979bb99e93ba3bf6d2a651
User <user@example.com>
2026-07-30 00:40:47 -0400

    not lazy loading mason since it messes up path

--- plugin/mason.lua
+++ plugin/mason.lua
@@ -1,9 +1,2 @@
-vim.api.nvim_create_user_command('Mason', function()
-    -- so that mason can reregister the mason command to open mason
-    vim.api.nvim_del_user_command('Mason')
-
-    vim.pack.add { "https://github.com/mason-org/mason.nvim" }
-    require('mason').setup {}
-
-    vim.cmd('Mason')
-end, {})
+vim.pack.add { "https://github.com/mason-org/mason.nvim" }
+require('mason').setup {}
